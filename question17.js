267c044582ff1df2537fabb0344ffab930c0afad
Davidmurris <141466955+Davidmurris@users.noreply.github.com>
2026-04-02 21:03:55 +0100

    Update question17.js

--- question17.js
+++ question17.js
@@ -1,13 +1,4 @@
 let input = 'javascript';
-let t = input.slice(9,10)
-let p = input.slice(8,9)
-let i = input.slice(7,8)
-let r = input.slice(6,7)
-let c = input.slice(5,6)
-let s = input.slice(4,5)
-let a1 = input.slice(3,4)
-let v = input.slice(2,3)
-let a2 = input.slice(1,2)
-let j = input.slice(0,1)
+let reversed = input.split('').reverse().join('')
 
-console.log(t+p+i+r+c+s+a1+v+a2+j)
+console.log(reversed)
